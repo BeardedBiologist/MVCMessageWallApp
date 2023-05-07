088234e4e8b51afdf603ce41794a0ea1a4d29e60
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-07 12:00:07 +0200

    created messagemodel and added required fields

--- MVCMessageWall/Controllers/MessageWallController.cs
+++ MVCMessageWall/Controllers/MessageWallController.cs
@@ -30,6 +30,8 @@ namespace MVCMessageWall.Controllers
         {
             try
             {
+                // TO DO: Add insert logic here
+
                 return RedirectToAction(nameof(Index));
             }
             catch
--- MVCMessageWall/Models/MessageModel.cs
+++ MVCMessageWall/Models/MessageModel.cs
@@ -1,7 +1,13 @@
-﻿namespace MVCMessageWall.Models
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
+
+namespace MVCMessageWall.Models
 {
     public class MessageModel
     {
+        [Required]
+        [StringLength(10, MinimumLength = 5)]
         public string Message { get; set; }
     }
 }
